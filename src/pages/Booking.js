a5b15b3731f8967bb20dc35205bb2b2296925d4c
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-05-13 11:00:17 +0530

    first commit

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -6,10 +6,10 @@ function Booking() {
         <div className='booking_bg w-screen '>
             <Navbar />
             <h1>Booking</h1>
-            <div className="flex justify-evenly mt-10 h-[91vh]">
+            <div className="flex justify-evenly mt-10 lg:h-[91vh] md:h-full h-[100%] flex-wrap">
                 <div className='shadow-lg shadow-black/20 '>
-                    <form className="w-full max-w-lg m-10 bg-black/20 backdrop-blur-[1px]" >
-                        <div className="flex flex-wrap justify-between -mx-3 mb-6 m-10" >
+                    <form className="w-full max-w-lg lg:m-10 bg-black/20 backdrop-blur-[1px] " >
+                        <div className="flex flex-wrap justify-between mx-3 mb-6 m-10 lg:m-10 md:mx-[20px] md:m-[20px]" >
                             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label className="block uppercase tracking-wide text-white text-m font-bold mb-2" for="grid-first-name">
                                     First Name
@@ -76,7 +76,7 @@ function Booking() {
                 </div>
                 <div>
                     {/* PRicing List */}
-                    <div className="bg-black/20 w-[28vw]  shadow overflow-hidden backdrop-blur-[1px] sm:rounded-lg">
+                    <div className="bg-black/20 lg:w-[28vw]  shadow overflow-hidden backdrop-blur-[1px] sm:rounded-lg">
                         <div className="px-4 py-5 sm:px-6">
                             <h3 className="text-2xl text-center leading-6 font-medium text-white ">
                                 Pricing
